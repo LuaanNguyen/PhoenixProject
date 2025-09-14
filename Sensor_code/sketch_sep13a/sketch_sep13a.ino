8457e1803cae13a98602806aef6ce5ab50855f39
flaa-ux <fansencf@mit.edu>
2025-09-14 10:19:32 -0400

    Add files via upload

---
+++ Sensor_code/sketch_sep13a/sketch_sep13a.ino
@@ -0,0 +1,37 @@
+/*********
+  Rui Santos
+  Complete project details at https://randomnerdtutorials.com  
+  Based on the Dallas Temperature Library example
+*********/
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is conntec to the Arduino digital pin 8
+#define ONE_WIRE_BUS 8
+
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+
+void setup(void)
+{
+  // Start serial communication for debugging purposes
+  Serial.begin(9600);
+  // Start up the library
+  sensors.begin();
+}
+
+void loop(void){ 
+  sensors.requestTemperatures(); 
+  
+  Serial.print("Time: ");
+  Serial.print(millis());
+  Serial.print(" ms - Celsius temperature: ");
+  Serial.print(sensors.getTempCByIndex(0)); 
+  Serial.print(" - Fahrenheit temperature: ");
+  Serial.println(sensors.getTempFByIndex(0));
+  delay(1); // Changed to 1 second for readability
+}
